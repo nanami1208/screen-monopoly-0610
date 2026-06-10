948bfbfda97fffd69e0768cf4098910ae35b9ba9
nanami1208 <n73.622.1208@gmail.com>
2026-06-11 03:28:44 +0900

    Add files via upload

---
+++ graphs/final_html/monthly_diversity_indices.js
@@ -0,0 +1,57 @@
+const monthlyDiversityIndices = [
+  {
+    "month": "2026-01",
+    "movie_count": 38,
+    "HHI_screen": 0.1089740148381833,
+    "HHI_screen_10000": 1089.7401483818337,
+    "effective_movie_count": 9.176499567212517,
+    "CR1_screen_pct": 19.983017103383386,
+    "CR3_screen_pct": 48.49936601010941,
+    "CR5_screen_pct": 66.04797668294194,
+    "CR10_screen_pct": 86.09640088815958
+  },
+  {
+    "month": "2026-02",
+    "movie_count": 35,
+    "HHI_screen": 0.1417472262191303,
+    "HHI_screen_10000": 1417.4722621913031,
+    "effective_movie_count": 7.054811770736711,
+    "CR1_screen_pct": 30.71440119781525,
+    "CR3_screen_pct": 55.16979593558228,
+    "CR5_screen_pct": 69.99486851447007,
+    "CR10_screen_pct": 83.29244546917946
+  },
+  {
+    "month": "2026-03",
+    "movie_count": 41,
+    "HHI_screen": 0.1997426044749969,
+    "HHI_screen_10000": 1997.426044749969,
+    "effective_movie_count": 5.006443180354027,
+    "CR1_screen_pct": 39.62203806626772,
+    "CR3_screen_pct": 65.7460448323407,
+    "CR5_screen_pct": 75.23631504882661,
+    "CR10_screen_pct": 85.74149621185201
+  },
+  {
+    "month": "2026-04",
+    "movie_count": 42,
+    "HHI_screen": 0.1279756644136355,
+    "HHI_screen_10000": 1279.7566441363551,
+    "effective_movie_count": 7.813985608762757,
+    "CR1_screen_pct": 20.45873186972124,
+    "CR3_screen_pct": 58.79327047715209,
+    "CR5_screen_pct": 69.02604756511892,
+    "CR10_screen_pct": 84.89336735390032
+  },
+  {
+    "month": "2026-05",
+    "movie_count": 25,
+    "HHI_screen": 0.1145308798373179,
+    "HHI_screen_10000": 1145.3087983731798,
+    "effective_movie_count": 8.731269692683934,
+    "CR1_screen_pct": 17.340464502893983,
+    "CR3_screen_pct": 48.682687376364576,
+    "CR5_screen_pct": 69.15158619679099,
+    "CR10_screen_pct": 91.47483332112245
+  }
+];
